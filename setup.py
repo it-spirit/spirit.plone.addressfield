f3c4bbd63a5f07e673a3eb3a0bb1329fa3b6e042
Thomas Massmann <thomas.massmann@it-spir.it>
2021-12-13 08:18:52 -0500

    style: :art: run isort and black

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 """Installer for the spirit.plone.addressfield package."""
 
-from setuptools import find_packages, setup
+from setuptools import find_packages
+from setuptools import setup
+
 
 long_description = "\n\n".join(
     [
